b8c8a0a3c127e36ed108d4825e9ed0a4b8351fbb
Hong Hanh <honghanh@pop-os.localdomain>
2021-06-17 20:08:57 +0700

    add normalize & test for linear_regression

--- HW5 - Linear Regression, Logistic Regression & Softmax Regression/linear_regression.py
+++ HW5 - Linear Regression, Logistic Regression & Softmax Regression/linear_regression.py
@@ -2,30 +2,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 np.random.seed(0)
 
-def linear_regression(X, y, lr, epochs=1500):
+def normalize(X):
+    return (X - X.mean(axis=0)) / X.std(axis=0)
+
+def linear_regression(X, y, lr, epochs=100):
     """
     """
     m, n = X.shape
     W = np.random.randn(n)
-    b = 0
+    b = 0.0
     epoch = 0  
     loss_list = []
-
+    # normalize data
+    X = normalize(X)
     while True:
-        if epoch > epochs - 1:
-            break
         h = X @ W + b
         loss = np.sum(np.square(h - y)) / (2*m)
         loss_list.append(loss)
-        #dW = (X.T @ (h - y)) / m
         dW = (X.T @ (h - y)) / m
         db = np.sum(h - y) / m
         W = W - lr * dW
         b = b - lr * db
         epoch += 1
-    return X @ W + b, loss_list
+        if epoch > epochs - 1:
+            break
+    return W, b, loss_list
 
 def lr_visualize_loss(loss_list):
     sns.lineplot(x=range(len(loss_list)), y=loss_list)
@@ -36,3 +40,12 @@ def visualize(X, y, predict):
     plt.plot(X, predict)
     plt.show()
     plt.close()
+
+def test():
+    from sklearn.datasets import load_boston
+    from sklearn.model_selection import train_test_split
+    X, y = load_boston(return_X_y=True)
+    a, b = linear_regression(X, y, lr=0.01, epochs=100)
+    print(a)
+if __name__ == '__main__':
+    test()
